2225d17fcb0be9c9764275a3c3a230e9f7c614d6
Agus3160 <ampharos321@hotmail.com>
2024-12-01 15:15:25 -0300

    Todas las fases completadas y nav configurado

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const topButton = document.getElementById("top-button");
 
+const seccionesIds = ["empatizar", "definir", "idear", "prototipar"];
+const titles = seccionesIds.map((id) => `title-${id}`);
+
 topButton.onclick = () => {
   window.scrollTo({
     top: 0,
@@ -15,4 +18,22 @@ window.addEventListener("scroll", () => {
     topButton.classList.remove("opacity-100");
     topButton.classList.add("opacity-0");
   }
-})
+});
+
+const configNavItems = () => {
+  const titulosEl = titles.map((id) => document.getElementById(id));
+  seccionesIds.forEach((id, index) => {
+    const seccion = document.getElementById(id);
+    if (seccion != null)
+      seccion.onclick = () => {
+        window.scrollTo({
+          top: titulosEl[index].offsetTop - 25,
+          behavior: "smooth",
+        });
+      };
+  });
+};
+
+window.onload = () => {
+  configNavItems();
+};
